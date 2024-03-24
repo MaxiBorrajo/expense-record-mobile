68c275c657b7c66780f56114604dab98f316a834
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2024-03-24 14:47:25 -0300

    fix

---
+++ app.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    build: {
+      preview: {
+        android: {
+          buildType: 'apk'
+        }
+      },
+      preview2: {
+        android: {
+          gradleCommand: ':app:assembleRelease'
+        }
+      },
+      preview3: {
+        developmentClient: true
+      },
+      preview4: {
+        distribution: 'internal'
+      },
+      production: {
+        env: {
+          EXPO_PUBLIC_URL_BACKEND: 'https://expense-record-production.up.railway.app/api',
+          EXPO_PUBLIC_ANDROID_CLIENT_ID: '440036428069-sqko1un37nvkki6vm0uo1oe50bjcmc59.apps.googleusercontent.com',
+          EXPO_PUBLIC_GOOGLE_SERVICES_JSON: 'google-services.json'
+        }
+      }
+    }
+  };
+  
