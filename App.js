6b0431c30368aa8fa7f798df4eb797ec39f48024
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2024-03-26 21:19:33 -0300

    commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'expo-dev-client'
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HeroScreen from "./src/screens/HeroScreen";
